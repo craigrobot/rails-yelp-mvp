2138726e0f2f4c5fe5b5300500459fd50268e6e7
Craig Horan <craighoran8@gmail.com>
2020-08-13 17:54:47 -0700

    Completed index route, action, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see get    "restaurants",          to: "restaurants#index",  as: :restaurants
+  # GET "restaurants"
+  get "/restaurants", to: "restaurants#index", as: :restaurants
+  # GET "restaurants/38"
+  get "/restaurants/:id", to: "restaurants#show", as: :restaurant
+
+
 end
